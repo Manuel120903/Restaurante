24133766812e403451fa53ed3066cd7361e41d92
Carmen <2122100666@soy.utj.edu.mx>
2023-09-22 09:20:18 -0600

    informaciòn en los campos mas o menos, pendiente

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,63 +1,69 @@
-<form class="row g-3">
-    <div class="col-2">
-          {{-- separador de columnas --}}
-    </div>
-    <div class="col-md-4">
-      <label for="validationDefault01" class="form-label">Nombre del Producto</label>
-      <input type="text" class="form-control" id="validationDefault01" value="Mark" required>
-    </div>
-    <div class="col-md-4">
-      <label for="validationDefault02" class="form-label">Last name</label>
-      <input type="text" class="form-control" id="validationDefault02" value="Otto" required>
-    </div>
-    <div class="col-3">
-              {{-- separador de columnas --}}
-    </div>
-    <div class="col-md-3">
-      <label for="validationDefaultUsername" class="form-label">Username</label>
-      <div class="input-group">
-        <span class="input-group-text" id="inputGroupPrepend2">@</span>
-        <input type="text" class="form-control" id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" required>
+<div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <form class="row g-2 needs-validation" novalidate>
+          <div class="col-2">
+            {{-- separador de columnas --}}
+          </div>
+          <div class="col-md-4">
+            <label for="validationDefault01" class="form-label">Nombre del platillo</label>
+            <input type="text" class="form-control" id="validationDefault01" placeholder="Escribe un platillo" required>
+          </div>
+
+          <div class="col-md-3">
+            <label for="validationDefault04" class="form-label">Categoria</label>
+            <select class="form-select" id="validationDefault04" required>
+              <option selected disabled value="">Elige una opción</option>
+              <option>Desayuno</option>
+              <option>Comida</option>
+              <option>Entrada</option>
+              <option>Postre</option>
+              <option>Bebida</option>
+            </select>
+          </div>
+
+          <div class="col-md-3">
+            <label for="validationDefault02" class="form-label">Descripción</label>
+            <input type="text" class="form-control" id="validationDefault02" placeholder="Escribe una descripción" required>
+          </div>
+
+          <div class="col-5">
+            {{-- separador de columnas --}}
+          </div>
+
+          <div class="col-md-3">
+            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+            <label class="form-check-label" for="defaultCheck1">
+              Default checkbox
+            </label>
+          </div>
+
+ 
+
+          <div class="col-5">
+            {{-- separador de columnas --}}
+          </div>
+
+          <div class="col-5">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+              <label class="form-check-label" for="invalidCheck2">
+                Agree to terms and conditions
+              </label>
+            </div>
+          </div>
+
+          <div class="col-6">
+            {{-- separador de columnas --}}
+          </div>
+
+          <div class="col-4">
+            <button class="btn btn-danger" type="submit">Registrar</button>
+          </div>
+        </form>
       </div>
     </div>
-    
-    <div class="col-md-3">
-      <label for="validationDefault03" class="form-label">City</label>
-      <input type="text" class="form-control" id="validationDefault03" required>
-    </div>
-    <div class="col-4">
-          {{-- separador de columnas --}}
-    </div>
-    <div class="col-md-3">
-      <label for="validationDefault04" class="form-label">State</label>
-      <select class="form-select" id="validationDefault04" required>
-        <option selected disabled value="">Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="col-md-3">
-      <label for="validationDefault05" class="form-label">Zip</label>
-      <input type="text" class="form-control" id="validationDefault05" required>
-    </div>
-    <div class="col-5">
-      {{-- separador de columnas --}}
-    </div>
-    <div class="col-5">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
-        <label class="form-check-label" for="invalidCheck2">
-          Agree to terms and conditions
-        </label>
-      </div>
-    </div>
-    <div class="col-6">
-          {{-- separador de columnas --}}
-    </div>
-    <div class="col-4">
-      <button class="btn btn-danger" type="submit">Submit form</button>
-    </div>
-  </form>
-  
+  </div>
   <script>
       // Example starter JavaScript for disabling form submissions if there are invalid fields
   (() => {
